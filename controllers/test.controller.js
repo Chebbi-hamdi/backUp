280794563c642791a7e0347a1c15bfdd49a2cf25
chebbihamdioff@gamil.com <chebbihamdioff@gamil.com>
2024-06-02 11:44:52 +0100

    14

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -19,7 +19,17 @@ const createTest = async (req, res) => {
     }
     };
 
+    const getTest = async (req, res) => {
+        try {
+            const tests = await Test.find();
+            res.status(200).json(tests);
+        } catch (error) {
+            console.error("Error getting tests:", error);
+            res.status(500).json({ message: "Internal server error" });
+        }
+    };
 
     export default {
         createTest,
+        getTest,
     };
