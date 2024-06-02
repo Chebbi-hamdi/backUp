f0e24ea4cf4136886ff16eb3329bed2e80610dad
chebbihamdioff@gamil.com <chebbihamdioff@gamil.com>
2024-06-02 10:33:38 +0100

    04

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,7 +1,11 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/user");
-// const FacebookStrategy = require("passport-facebook").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const dotenv = require("dotenv");
+const moment = require("moment");
+const jwt = require("jsonwebtoken");
+
 /**
  * Configures Passport to use Google OAuth 2.0 for authentication.
  */
@@ -9,29 +13,50 @@ const User = require("../models/user");
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/public/auth/google/callback",
-      scope: ["profile", "email"],
+      clientID:
+        "277304945553-fdslunliksd0mf9rct405qp7nn6jmsi4.apps.googleusercontent.com",
+      clientSecret: "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",
+      callbackURL: "http://localhost:3000/api/v0/auth/google/callback",
+      scope: [
+        "profile",
+        "email",
+        "https://www.googleapis.com/auth/userinfo.profile",
+      ],
     },
     async (accessToken, refreshToken, profile, done) => {
+      console.log(
+        "------------------------profile:------------------",
+        profile
+      );
+      const profileData = {
+        googleId: profile.id,
+        email: profile.emails[0].value,
+        imagePath: profile.photos[0].value,
+        name: profile.displayName,
+        familyName: profile.name.familyName,
+      };
+      console.log("profileData:", profileData); // Log the profile data
       try {
         let user = await User.findOne({
-          "email.primary": profile.emails[0].value,
+          "email.primary": profileData.email,
         });
 
-        if (user) {
-          return done(null, user);
+        console.log("user%%%%%%%%%%%%%%%:", user.name); // Log the user data
+        const playload = {
+          exp: moment()
+            .add(process.env.JWT_EXPIRATION_MINUTES, "minutes")
+            .unix(),
+          iat: moment().unix(),
+          sub: user._id,
+        };
+        const access = jwt.sign(playload, process.env.JWT_TOKEN_SECRET);
+        if (!user.googleAuth) {
+          user.googleAuth = {};
         }
-
-        user = new User({
-          googleId: profile.id,
-          "email.primary": profile.emails[0].value,
-          authType: "google",
-        });
+        user.googleAuth.accessToken = access;
         await user.save();
-
-        done(null, user);
+        console.log("user://////////////////", user); // Log the user data
+        return done(null, user);
       } catch (error) {
         done(error);
       }
@@ -61,39 +86,39 @@ passport.deserializeUser(async (id, done) => {
  * Configures Passport to use Facebook OAuth 2.0 for authentication.
  */
 
-// passport.use(
-//   new FacebookStrategy(
-//     {
-//       clientID: process.env.FACEBOOK_APP_ID,
-//       clientSecret: process.env.FACEBOOK_APP_SECRET,
-//       callbackURL: "/public/auth/facebook/callback",
-//       profileFields: ["id", "emails", "name"], 
-//     },
-//     async (accessToken, refreshToken, profile, done) => {
-//       try {
-//         let user = await User.findOne({
-//           "email.primary": profile.emails[0].value,
-//         });
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: "/public/auth/facebook/callback",
+      profileFields: ["id", "emails", "name"], 
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({
+          "email.primary": profile.emails[0].value,
+        });
 
-//         if (user) {
-//           return done(null, user);
-//         }
+        if (user) {
+          return done(null, user);
+        }
 
-//         user = new User({
-//           facebookId: profile.id,
-//           "email.primary": profile.emails[0].value,
-//           authType: "facebook",
-//         });
+        user = new User({
+          facebookId: profile.id,
+          "email.primary": profile.emails[0].value,
+          authType: "facebook",
+        });
 
-//         await user.save();
+        await user.save();
 
-//         done(null, user);
-//       } catch (error) {
-//         done(error);
-//       }
-//     }
-//   )
-// );
+        done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
